0b705bcb43d4bffa65537ccc1caccf29a3c77469
King Simbol <2736389388@qq.com>
2024-01-24 11:32:22 +0800

    create menu -1

--- game/urls/urls.py
+++ game/urls/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
-from game import views
+from django.urls import path, include
+from game.views.views import index
+
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', index, name='index'),
+    path('/menu/', include('game.urls.menu.urls')),
+    path('/playground/', include('game.urls.playground.urls')),
+    path('/settings/', include('game.urls.settings.urls'))
 ]
