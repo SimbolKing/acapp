80ba89e8b775fa596c919512ecc452ccf3598265
SimbolKing <2736389388@qq.com>
2026-03-30 09:58:23 +1100

    start the project, add homepage and playground

---
+++ game/static/js/src/menu/zbase.js
@@ -0,0 +1,54 @@
+class AcGameMenu {
+    constructor(root) {
+        this.root = root;
+        this.$menu = $(`
+<div class="ac-game-menu">
+    <div class="ac-game-menu-field">
+        <div class="ac-game-menu-field-item ac-game-menu-field-item-single-mode">
+            Single
+        </div>
+        <br>
+        <div class="ac-game-menu-field-item ac-game-menu-field-item-multi-mode">
+            Multi
+        </div>
+        <br>
+        <div class="ac-game-menu-field-item ac-game-menu-field-item-settings">
+            Settings
+        </div>
+    </div>
+</div>
+`);
+        this.root.$ac_game.append(this.$menu);
+        this.$single_mode = this.$menu.find('.ac-game-menu-field-item-single-mode');
+        this.$multi_mode = this.$menu.find('.ac-game-menu-field-item-multi-mode');
+        this.$settings = this.$menu.find('.ac-game-menu-field-item-settings');
+
+        this.start();
+    }
+
+    start() {
+        this.add_listening_events();
+    }
+
+    add_listening_events() {
+        let outer = this;
+        this.$single_mode.click(function(){
+            outer.hide();
+            outer.root.playground.show();
+        });
+        this.$multi_mode.click(function(){
+            console.log("click multi mode");
+        });
+        this.$settings.click(function(){
+            console.log("click settings");
+        });
+    }
+
+    show() {
+        this.$menu.show();
+    }
+
+    hide() {
+        this.$menu.hide();
+    }
+}
